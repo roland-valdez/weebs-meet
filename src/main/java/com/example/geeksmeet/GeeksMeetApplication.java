2e9d6fd3c457aa677a5cb946d2d20910cfcdb182
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-07-26 15:18:37 -0500

    Feat: starting new project

---
+++ src/main/java/com/example/geeksmeet/GeeksMeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.geeksmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeksMeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeeksMeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/geeksmeet/GeeksMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geeksmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeksMeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
