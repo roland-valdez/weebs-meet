3494706895db055cb25995759addc0cecc6e1e57
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-07-28 15:00:00 -0500

    feat: conversation model created

---
+++ src/main/java/Models/Conversation.java
@@ -0,0 +1,19 @@
+package Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="conversation")
+public class Conversation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
