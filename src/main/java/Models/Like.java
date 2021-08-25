826adf11767ebf7c977aeab8eed4a4a5e8eef681
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-25 15:31:26 -0500

    feat: Like model complete

--- src/main/java/Models/Like.java
+++ src/main/java/Models/Like.java
@@ -1,6 +1,7 @@
 package Models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="like")
@@ -8,6 +9,28 @@ public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @OneToMany(cascade= CascadeType.ALL, mappedBy = "user")
+    private List<User> users;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "post")
+    private List<Post> posts;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 
     public long getId() {
         return id;
