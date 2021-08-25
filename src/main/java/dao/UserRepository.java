84b27bb756c23f80c534326187dc6827f30c5196
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-25 16:41:18 -0500

    feat: starting repository

---
+++ src/main/java/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package dao;
+
+import Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
