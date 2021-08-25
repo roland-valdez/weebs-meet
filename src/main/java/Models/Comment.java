5fcb63d67608181ee359e329f57090808cd56e0d
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-25 12:24:02 -0500

    Fear: comment model

--- src/main/java/Models/Comment.java
+++ src/main/java/Models/Comment.java
@@ -11,11 +11,39 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = true, length = 255)
     private String comment;
 
     @Column(name="comment_created")
     private Timestamp comment_created;
+
+    @ManyToOne
+    @JoinColumn(name= "post_id", referencedColumnName = "id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", referencedColumnName = "id")
+    private User user;
+
+
+    //constructor
+    public Comment(){
+    }
+
+    public Comment(String comment, User user, Post post, Timestamp comment_created){
+        this.comment= comment;
+        this.user= user;
+        this.post= post;
+        this.comment_created = comment_created;
+    }
+
+    public Comment(long id, String comment, Post post, User user, Timestamp comment_created){
+        this.comment_created = comment_created;
+        this.id = id;
+        this.comment = comment;
+        this.user= user;
+        this.post=post;
+    }
 
     public long getId() {
         return id;
@@ -39,5 +67,21 @@ public class Comment {
 
     public void setComment_created(Timestamp comment_created) {
         this.comment_created = comment_created;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
