d93612491f81703367adb43f185f107b0755b4e1
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-29 16:12:05 -0500

    feat: sender and reciever added

--- src/main/java/Models/Message.java
+++ src/main/java/Models/Message.java
@@ -18,7 +18,11 @@ public class Message {
 
     @ManyToOne
     @JoinColumn(name= "user")
-    private User user;
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name= "user")
+    private User receiver;
 
    public Message(){
 
@@ -27,11 +31,18 @@ public class Message {
    public Message(String text){
        this.text=text;
    }
-   public Message(long id, String text, Timestamp timestamp, User user){
+    public Message( String text, Timestamp timestamp, User sender,User receiver){
+        this.text=text;
+        this.timestamp=timestamp;
+        this.sender = sender;
+        this.receiver=receiver;
+    }
+   public Message(long id, String text, Timestamp timestamp, User sender,User receiver){
        this.id=id;
        this.text=text;
        this.timestamp=timestamp;
-       this.user = user;
+       this.sender = sender;
+       this.receiver=receiver;
    }
 
     public long getId() {
@@ -58,11 +69,19 @@ public class Message {
         this.timestamp = timestamp;
     }
 
-    public User getUser() {
-        return user;
+    public User getSender() {
+        return sender;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
     }
 }
